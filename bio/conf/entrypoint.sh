d035413f5b3a6dfdd2bac8e6ebd912e16c1d771f
Your Name <you@example.com>
2017-02-09 07:55:02 +0000

    bio

--- bio/conf/entrypoint.sh
+++ bio/conf/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-export YARN_IDENT_STRING=root
-export HADOOP_SSH_OPTS="-F /root/.ssh/config"
-chown -R root:root /hadoop
-
-
 if [ "" = "$MASTER_HOSTNAME" ]; then
     MASTER_HOSTNAME=$HOSTNAME
 fi
@@ -20,8 +14,8 @@ sed -i "s/HOSTNAME_HADOOP/${HOSTNAME}/g" hdfs-site.xml
 sed -i "s/HOSTNAME_HADOOP/${HOSTNAME}/g" mapred-site.xml
 sed -i "s/HOSTNAME_HADOOP/${HOSTNAME}/g" yarn-site.xml
 
-sed -i "s#<value>1</value>#<value>4</value>#g" hdfs-site.xml
-sed -i "s#</configuration>#<property><name>dfs.balance.bandwidthPerSec</name><value>10485760</value></property></configuration>#g" hdfs-site.xml
+#sed -i "s#<value>1</value>#<value>4</value>#g" hdfs-site.xml
+#sed -i "s#</configuration>#<property><name>dfs.balance.bandwidthPerSec</name><value>10485760</value></property></configuration>#g" hdfs-site.xml
 #sed -i "s#<value>/data/hdfs/data</value>#<value>/data/hdfs/data1,/data/hdfs/data2,/data/hdfs/data3,/data/hdfs/data4</value>#g" core-site.xml
 if [ ! -d "/data/hdfs" ]; then
     mkdir /data/hdfs && chown -R hduser:hadoop /data/hdfs
@@ -44,6 +38,3 @@ fi
 /hadoop/sbin/yarn-daemon.sh start nodemanager
 
 tail -f /spark/logs/*
-
-#export SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY:-128M}"
-#export SPARK_HOME="/spark"
