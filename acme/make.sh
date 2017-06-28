bcc3bd63c9c7703daf9a00a818896d9b3f803271
Your Name <you@example.com>
2017-06-28 13:15:25 +0800

    acme_tiny

--- acme/make.sh
+++ acme/make.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [ "$1" == "" ] || [ "$2" == "" ]; then
-    echo make.sh "domain" "DNS:domain.com,DNS:www.domain.com"
+if [ "$(whoami)" != "root" ];then
+    echo "need root"
+    exit
+fi
+
+if [ "$1" == "" ] ; then
+    echo "sudo make.sh domain.com"
     exit
 fi
 
@@ -13,32 +18,43 @@ cd $ACME_PATH
 if [ ! -f "$ACME_PATH/account.key" ]; then
     openssl genrsa 4096 > account.key
     openssl genrsa 4096 > domain.key
-    openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=$2")) > domain.csr
+    openssl req -new -sha256 -key domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:$1")) > domain.csr
     openssl req -new -sha256 -key domain.key -out domain.csr
+
+echo "write nginx conf:"
+echo "
+server {
+    server_name $1;
+    root /www/server;
+
+    location ^~ /.well-known/acme-challenge/ {
+        alias /www/acme/$1/;
+    }
+
+    include php.conf;
+}
+"
+exit 0
 fi
 
-docker run -it --rm -v $ACME_PATH:/www ivories/acme python /acme_tiny.py --account-key /www/account.key --csr /www/domain.csr --acme-dir /www/ > $ACME_PATH/signed.crt
+docker run -it --rm -v $ACME_PATH:/www ivories/acme python /acme_tiny.py --account-key /www/account.key --csr /www/domain.csr --acme-dir /www/ > $ACME_PATH/signed.crt || exit
 
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
 cat signed.crt intermediate.pem > chained.pem
 wget -O - https://letsencrypt.org/certs/isrgrootx1.pem > root.pem
 cat intermediate.pem root.pem > full_chained.pem
 
-systemctl restart nginx
-
-echo 'write nginx conf:'
-echo 'server {
-    server_name domain.com;
-
-    location ^~ /.well-known/acme-challenge/ {
-        alias $ACME_PATH;
-    }
-}
+echo "
 server {
-    server_name domain.com;
+    server_name $1;
+    root /www/$1;
     listen 443;
     ssl on;
-    ssl_certificate     $ACME_PATH/chained.pem;
-    ssl_certificate_key $ACME_PATH/domain.key;
+    ssl_certificate     /www/acme/$1/chained.pem;
+    ssl_certificate_key /www/acme/$1/domain.key;
+    include php.conf;
 }
-'
+"
+
+systemctl restart nginx
+
